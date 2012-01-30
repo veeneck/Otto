6afcc2a82390da11d5c056af9d8b4915fa211b01
gameandcode <veeneck@gmail.com>
2012-01-29 21:58:42 -0800

    working with layers and loading resources through json

--- screens/menu.py
+++ screens/menu.py
@@ -5,7 +5,7 @@ import lib.core
 
 class MainMenu(Base):
 
-    menu_group = pygame.sprite.Group()
+    menu_group = pygame.sprite.LayeredUpdates()
 
     def __init__(self):
         Base.__init__(self)
@@ -18,22 +18,24 @@ class MainMenu(Base):
             for event in pygame.event.get():
                 self.handle_core_events(event)
 
-
             self.menu_group.draw(screen) # draw everything in the group
             pygame.display.flip()
             
     def load_resources(self):
 
-        # Logo image
-        logo = pygame.sprite.Sprite()
-        logo.image = pygame.image.load("resources/otto_logo.png")
-        logo.rect = logo.image.get_rect()
-        logo.rect.topleft = [200, 125]
+        resources = {
+            'bg' : {'layer' : 0, 'topleft' : [0, 0], 'image' : 'otto_interface_bg.jpg'},
+            'logo' : {'layer' : 1, 'topleft' : [200, 125], 'image' : 'otto_logo.png'},
+            'play_icon' : {'layer' : 1, 'topleft' : [615, 414], 'image' : 'otto_interface_x.png'},
+            'option_icon' : {'layer' : 1, 'topleft' : [615, 464], 'image' : 'otto_interface_x.png'},
+            'quit_icon' : {'layer' : 1, 'topleft' : [615, 514], 'image' : 'otto_interface_x.png'}       
+        }
 
-        # Background image
-        bg = pygame.sprite.Sprite()
-        bg.image = pygame.image.load("resources/otto_interface_bg.jpg")
-        bg.rect = bg.image.get_rect()
-        bg.rect.topleft = [0, 0]
-
-        self.menu_group.add(bg, logo)
+        for assets in resources:
+            asset = resources[assets]
+            spr = pygame.sprite.Sprite()
+            spr.image = pygame.image.load('resources/' + asset['image'])
+            spr.rect = spr.image.get_rect()
+            spr.rect.topleft = asset['topleft']
+            spr._layer = asset['layer']
+            self.menu_group.add(spr)
